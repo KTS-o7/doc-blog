8daa27375300b8b20bd17f942277cf2d189f9862
krishnatejaswis <shentharkrishnatejaswi@gmail.com>
2025-09-19 23:16:15 +0530

    blog update 2025-09-19 - gray_code.py,

--- gray_code.py
+++
@@ -1,9 +0,0 @@
-def main():
-    n = int(input())
-    for i in range(2**n):
-        num = i^(i>>1)
-        num_str = bin(num)[2:].zfill(n)
-        print(num_str, end="\n")
-
-if __name__ == "__main__":
-    main()
